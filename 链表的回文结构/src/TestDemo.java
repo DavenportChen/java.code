c33a8563addf4ad8b3ee6af3472e8096d34cd41d
DavenportChen <1163055357@qq.com>
2021-01-10 22:02:31 +0800

    .

---
+++ 链表的回文结构/src/TestDemo.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Descriptiion:
+ * User: ${陈文博}
+ * Date: 2021-01-09
+ * Time: 18:15
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addLast(5);
+        myLinkedList.addLast(4);
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(4);
+        myLinkedList.addLast(5);
+
+        myLinkedList.display();
+        System.out.println(myLinkedList.chkPalindrme());
+    }
+}
